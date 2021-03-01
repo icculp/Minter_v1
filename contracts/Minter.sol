8c56ffd7b94bb0f2af69e507d1a4b13b06e1e42d
Ian Culp <icculp@gmail.com>
2021-03-01 15:53:47 -0600

    Upgrading WSL

--- contracts/Minter.sol
+++ contracts/Minter.sol
@@ -1,8 +1,8 @@
 pragma solidity >=0.4.22 <0.9.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 
 contract Minter is ERC721 {
