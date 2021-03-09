384e3491b509b054071ecdf5a9a3dc94011612ec
Ian Culp <icculp@gmail.com>
2021-03-09 09:37:05 -0600

    Updating repo

---
+++ my-app/src/contracts/Minter.sol
@@ -0,0 +1,16 @@
+import "./ERC721Tradable.sol";
+
+contract Minter is ERC721Tradable {
+    constructor(address _proxyRegistryAddress)
+        public
+        ERC721Tradable("Creature", "OSC", _proxyRegistryAddress)
+    {}
+
+    function baseTokenURI() public pure returns (string memory) {
+        return "https://creatures-api.opensea.io/api/creature/";
+    }
+
+    function contractURI() public pure returns (string memory) {
+        return "https://creatures-api.opensea.io/contract/opensea-creatures";
+    }
+}
--- truffle-config.js
+++ truffle-config.js
@@ -43,7 +43,7 @@ module.exports = {
     // options below to some value.
     //
      development: {
-      host: "127.0.0.1",     // Localhost (default: none)
+      host: "172.17.106.81",     // Localhost (default: none)
       port: 7545,            // 7545 for Ganache test blockchain; 8545 isStandard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
      },
